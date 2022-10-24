03938f7b2aa236a52c1058f98b4c379311065670
JonathanYe3 <jonathanye2003@gmail.comgit config --global user.emailjonathanye2003@gmail.com>
2022-10-24 16:36:49 -0400

    Discovered a major issue with this script - was still using "temp" dataframe

--- Uganda/R/Uganda_PCA.R
+++ Uganda/R/Uganda_PCA.R
@@ -8,7 +8,7 @@ facetask_wide <- read_excel(path = Uganda_TwoTests, sheet = 3)
 # Facetask
 facetask <- facetask_wide[,sapply(facetask_wide, is.numeric)]
 facetask[,c("NumCompletions", "n", "go_n", "nogo_n")] <- NULL
-face_cor <- cor(temp, use = "pairwise.complete.obs") %>% scale()
+face_cor <- cor(facetask, use = "pairwise.complete.obs") %>% scale()
 
 face_pca <- psych::principal(facetask, nfactors = 10)
 base_face_pca <- prcomp(face_cor)
@@ -18,7 +18,7 @@ ggbiplot(base_face_pca)
 # Flanker
 flanker <- flanker_wide[,sapply(facetask_wide, is.numeric)]
 flanker[,c("NumCompletions", "n", "congruent_n", "incongruent_n")] <- NULL
-flank_cor <- cor(temp, use = "pairwise.complete.obs") %>% scale()
+flank_cor <- cor(flanker, use = "pairwise.complete.obs") %>% scale()
 
 flank_pca <- psych::principal(flank_cor)
 base_flank_pca <- prcomp(flank_cor)
