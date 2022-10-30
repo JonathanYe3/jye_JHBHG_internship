95eb7d55c61eda63cc1f42705e1ec510234b8c68
JonathanYe3 <jonathanye2003@gmail.comgit config --global user.emailjonathanye2003@gmail.com>
2022-10-30 19:56:37 -0400

    did some things

--- oct_28.R
+++ oct_28.R
@@ -10,7 +10,11 @@ facetask_wide <- read_excel(path = Uganda_TwoTests, sheet = 3)
 facetask <- facetask_wide[,sapply(facetask_wide, is.numeric)]
 facetask[,c("NumCompletions", "n", "go_n", "nogo_n", "session", "block")] <- NULL
 face_cor <- cor(facetask, use = "pairwise.complete.obs")
-face_cor <- reshape2::melt(face_cor)
+
+temp <- corr.test(as.matrix(facetask)) 
+# use these p values to make the stars
+
+face_cor <- reshape2::melt(face_cor) # use pivot longer instead
 
 ggplot(data = face_cor, aes(x=Var1,y=Var2, fill=value))+geom_tile()+
       scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
@@ -47,3 +51,34 @@ facetask[,c("NumCompletions", "n", "go_n", "nogo_n", "session", "block")] <- NUL
 facetask <- scale(facetask)
 
 face_pca <- psych::principal(facetask, nfactors = 6)
+plot.psych(face_pca)
+
+face_loadings <- face_pca[["loadings"]]
+face_loadings <- data.frame(matrix(as.numeric(face_loadings), attributes(face_loadings)$dim, 
+                  dimnames=attributes(face_loadings)$dimnames))
+# RC2 vs RC5
+ggplot(face_loadings, aes(x=RC2, y=RC5)) + 
+      geom_point(size = 2)+
+      ggtitle("Facetask: RC2 vs. RC5")
+
+ggscatterstats(face_loadings, x=RC2, y= RC5)+
+      ggtitle("Facetask: RC2 vs. RC5")
+
+# Flanker
+flanker <- flanker_wide[,sapply(flanker_wide, is.numeric)]
+flanker[,c("NumCompletions", "n", "congruent_n", "incongruent_n", "session", "block")] <- NULL
+flanker <- scale(flanker)
+
+flank_pca <- psych::principal(flanker, nfactors = 10)
+plot.psych(flank_pca)
+
+flank_loadings <- flank_pca[["loadings"]]
+flank_loadings <- data.frame(matrix(as.numeric(flank_loadings), attributes(flank_loadings)$dim, 
+                                   dimnames=attributes(flank_loadings)$dimnames))
+# RC1 vs RC2
+ggplot(flank_loadings, aes(x=RC1, y=RC2)) + 
+      geom_point(size = 2)+
+      ggtitle("Flanker: RC1 vs. RC2")
+
+ggscatterstats(flank_loadings, x=RC1, y= RC2)+
+      ggtitle("Flanker: RC1 vs. RC2")
