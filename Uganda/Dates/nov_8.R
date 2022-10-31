09c2d6c2a0bb863f6077a9345cf2b1038c5c7eee
JonathanYe3 <jonathanye2003@gmail.comgit config --global user.emailjonathanye2003@gmail.com>
2022-10-30 21:02:53 -0400

    Got some stuff done today :)

--- Uganda/Dates/nov_8.R
+++ Uganda/Dates/nov_8.R
@@ -1,1 +1,22 @@
 source("Uganda/R/setup.R")
+
+# Format data for PCA - get rid of cols that have NA - scale
+facetask_num <- facetask_num[ , colSums(is.na(facetask_num)) == 0] %>% scale()
+flanker_num <- flanker_num[ , colSums(is.na(flanker_num)) == 0] %>% scale()
+
+# Facetask PCA
+face_pca <- psych::principal(facetask_num, nfactors = 6)
+
+      # Scree Plot
+face_vaccounted <- face_pca[["Vaccounted"]] %>% t()
+face_vaccounted <- data.frame(matrix(as.numeric(face_vaccounted), attributes(face_vaccounted)$dim, 
+                  dimnames=attributes(face_vaccounted)$dimnames))
+
+p1 <- ggplot(data = face_vaccounted, aes(x= reorder(rownames(face_vaccounted), desc(Proportion.Var)), y=Proportion.Var))+
+      geom_bar(stat="identity", width=0.5)
+
+p2 <- ggplot(data = face_vaccounted, aes(x= reorder(rownames(face_vaccounted), desc(Proportion.Var)), y=Cumulative.Proportion, group=1))+
+      geom_line() + geom_point()
+p1+p2
+
+      
--- Uganda/R/setup.R
+++ Uganda/R/setup.R
@@ -1,4 +1,4 @@
-pacman::p_load("dplyr", "tidyr", "ggplot2", "ggstatsplot", "readxl", "corrplot", "reshape2", "psych")
+pacman::p_load("dplyr", "tidyr", "ggplot2", "ggstatsplot", "readxl", "corrplot", "reshape2", "psych", "patchwork")
 
 # Load data
 Uganda_TwoTests <- "Uganda/Data/Uganda_TwoTests.xlsx"
@@ -6,4 +6,7 @@ facetask_wide <- read_excel(path = Uganda_TwoTests, sheet = 3)
 flanker_wide <- read_excel(path = Uganda_TwoTests, sheet = 4)
 
 facetask_num <- facetask_wide[,sapply(facetask_wide, is.numeric)]
+facetask_num[,c("NumCompletions", "n", "go_n", "nogo_n", "session", "block")] <- NULL
+
 flanker_num <- flanker_wide[,sapply(facetask_wide, is.numeric)]
+flanker_num[,c("NumCompletions", "n", "congruent_n", "incongruent_n", "session", "block")] <- NULL
