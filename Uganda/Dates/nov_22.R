113af5eec8a4f5f7b0d33c03cd0e708d5e73b5ba
JonathanYe3 <jonathanye2003@gmail.comgit config --global user.emailjonathanye2003@gmail.com>
2022-11-19 16:26:24 -0500

    finished tasks for this week

--- Uganda/Dates/nov_22.R
+++ Uganda/Dates/nov_22.R
@@ -7,3 +7,9 @@ face_num <- uganda_list[["facetask_num"]]
 face_pca <- my_pca(face_num, 4)
 face_plots <- pca_plots(face_pca)
 face_heatmap <- pca_heatmap(face_pca)
+
+# flanker
+flank_num <- uganda_list[["flanker_num"]]
+flank_pca <- my_pca(flank_num, 3)
+flank_plots <- pca_plots(flank_pca)
+flank_heatmap <- pca_heatmap(flank_pca)
